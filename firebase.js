2a828fea29f7b819be9897ffeafa23d5c79d282b
1mVivek <gamingcodinggymrepeat@gmail.com>
2026-01-25 19:53:17 +0530

    Update Firebase config with new project settings
    Updated Firebase configuration with new project details.

--- firebase.js
+++ firebase.js
@@ -4,10 +4,13 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth
 
 // 🔥 Your Firebase Web App config
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  appId: "YOUR_APP_ID",
+  apiKey: "AIzaSyDvQnOqgKXcTuRopDQPA6z2wLpzrw9m4QY",
+  authDomain: "stock-managemet-app.firebaseapp.com",
+  projectId: "stock-managemet-app",
+  storageBucket: "stock-managemet-app.firebasestorage.app",
+  messagingSenderId: "414285111306",
+  appId: "1:414285111306:web:65356aa560ca4432b03ff3",
+  measurementId: "G-YX7TJPKBD5"
 };
 
 // Initialize Firebase
