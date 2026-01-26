c2a93406c9331bd7e78fb577b9d281d7f98de99f
1mVivek <gamingcodinggymrepeat@gmail.com>
2026-01-27 02:10:41 +0530

    Remove duplicate base configuration in vite.config.js
    Removed duplicate base configuration for GitHub Pages.

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/inventory-frontend/",
   plugins: [react()],
-  base: "/inventory-frontend/", // 🔥 VERY IMPORTANT for GitHub Pages
 });
