4df3f2ed409f700fa24a34354f6aeb3e29c9c65a
1mVivek <gamingcodinggymrepeat@gmail.com>
2026-01-28 00:28:56 +0530

    Update ColorModeContext.jsx

--- src/theme/ColorModeContext.jsx
+++ src/theme/ColorModeContext.jsx
@@ -1,6 +1,6 @@
 import { createContext, useMemo, useState, useEffect } from "react";
 import { doc, getDoc, updateDoc } from "firebase/firestore";
-import { db } from "../firebase";
+import { db } from "../services/firebase";
 import { getAuth } from "firebase/auth";
 
 export const ColorModeContext = createContext();
