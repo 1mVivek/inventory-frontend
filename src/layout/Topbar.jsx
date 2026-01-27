2fff7f49003c8282554f48bb6f06fe274a1f7abb
1mVivek <gamingcodinggymrepeat@gmail.com>
2026-01-27 12:56:19 +0530

    Update Topbar.jsx

--- src/layout/Topbar.jsx
+++ src/layout/Topbar.jsx
@@ -1,11 +1,40 @@
-import { AppBar, Toolbar, Button, Typography } from "@mui/material";
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  IconButton,
+  Button,
+} from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
+import { logout } from "../services/auth";
+import { useNavigate } from "react-router-dom";
 
-export default function Topbar({ setUser }) {
+export default function Topbar({ onMenuClick }) {
+  const navigate = useNavigate();
+
+  const handleLogout = async () => {
+    await logout();
+    localStorage.removeItem("token");
+    navigate("/login");
+  };
+
   return (
-    <AppBar position="static">
+    <AppBar position="fixed">
       <Toolbar>
-        <Typography sx={{ flex: 1 }}>Stock Management</Typography>
-        <Button color="inherit" onClick={() => setUser(null)}>
+        <IconButton
+          color="inherit"
+          edge="start"
+          onClick={onMenuClick}
+          sx={{ mr: 2, display: { md: "none" } }}
+        >
+          <MenuIcon />
+        </IconButton>
+
+        <Typography sx={{ flexGrow: 1 }}>
+          Stock Management
+        </Typography>
+
+        <Button color="inherit" onClick={handleLogout}>
           Logout
         </Button>
       </Toolbar>
