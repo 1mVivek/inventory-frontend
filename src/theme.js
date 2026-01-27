c237a2efa5697c8c215d8520ad6ea261eb70b012
1mVivek <gamingcodinggymrepeat@gmail.com>
2026-01-27 22:10:08 +0530

    Update theme.js

--- src/theme.js
+++ src/theme.js
@@ -4,11 +4,16 @@ export const getTheme = (mode) =>
   createTheme({
     palette: {
       mode,
+
       ...(mode === "dark"
         ? {
             background: {
               default: "#0b1020",
               paper: "#0f172a",
+            },
+            text: {
+              primary: "#e5e7eb",
+              secondary: "#9ca3af",
             },
           }
         : {
@@ -16,6 +21,20 @@ export const getTheme = (mode) =>
               default: "#f8fafc",
               paper: "#ffffff",
             },
+            text: {
+              primary: "#0f172a",
+              secondary: "#475569",
+            },
           }),
+    },
+
+    components: {
+      MuiAppBar: {
+        styleOverrides: {
+          root: {
+            backgroundImage: "none", // removes weird gradient
+          },
+        },
+      },
     },
   });
