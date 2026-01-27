47de589e897d7b31025334930e779db5422919a3
1mVivek <gamingcodinggymrepeat@gmail.com>
2026-01-27 20:59:53 +0530

    Create ThemeToggle.jsx

---
+++ src/components/ThemeToggle.jsx
@@ -0,0 +1,32 @@
+import { useContext } from "react";
+import { ColorModeContext } from "../theme/ColorModeContext";
+import "./theme-toggle.css";
+
+export default function ThemeToggle() {
+  const { toggleColorMode } = useContext(ColorModeContext);
+
+  return (
+    <label className="theme-switch">
+      <input
+        type="checkbox"
+        className="theme-switch__checkbox"
+        onChange={toggleColorMode}
+      />
+      <div className="theme-switch__container">
+        <div className="theme-switch__clouds"></div>
+        <div className="theme-switch__stars-container">
+          {/* SVG stays same */}
+        </div>
+        <div className="theme-switch__circle-container">
+          <div className="theme-switch__sun-moon-container">
+            <div className="theme-switch__moon">
+              <div className="theme-switch__spot"></div>
+              <div className="theme-switch__spot"></div>
+              <div className="theme-switch__spot"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </label>
+  );
+}
