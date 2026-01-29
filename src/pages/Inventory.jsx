3576ae7b11292d4e1380c6c921d67dbd6366a07d
1mVivek <gamingcodinggymrepeat@gmail.com>
2026-01-30 02:24:10 +0530

    Update Inventory.jsx

--- src/pages/Inventory.jsx
+++ src/pages/Inventory.jsx
@@ -1,26 +1,35 @@
-import { useState } from "react";
 import { Button } from "@mui/material";
+import { useEffect, useState } from "react";
 import ProductTable from "../components/ProductTable";
 import ProductModal from "../components/ProductModal";
+import { getItems } from "../services/api";
 
 export default function Inventory() {
   const [open, setOpen] = useState(false);
+  const [items, setItems] = useState([]);
+
+  async function load() {
+    const data = await getItems();
+    setItems(data);
+  }
+
+  useEffect(() => {
+    load();
+  }, []);
 
   return (
     <div style={{ padding: 20 }}>
-      <Button
-        variant="contained"
-        onClick={() => setOpen(true)}
-        sx={{ mr: 2 }}
-      >
+      <Button variant="contained" onClick={() => setOpen(true)}>
         Add New Product
       </Button>
 
-      <ProductTable />
-
+      <ProductTable items={items} />
       <ProductModal
         open={open}
-        onClose={() => setOpen(false)}
+        onClose={() => {
+          setOpen(false);
+          load();
+        }}
       />
     </div>
   );
