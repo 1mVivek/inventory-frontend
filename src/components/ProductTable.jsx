f8a086417c4568338d1bb8d2071f469ae41e7934
1mVivek <gamingcodinggymrepeat@gmail.com>
2026-01-30 02:54:49 +0530

    Update ProductTable.jsx

--- src/components/ProductTable.jsx
+++ src/components/ProductTable.jsx
@@ -4,33 +4,37 @@ import {
   TableRow,
   TableCell,
   TableBody,
+  Paper,
 } from "@mui/material";
 
 export default function ProductTable({ products }) {
-  if (!products.length) {
-    return <p>No products yet</p>;
+  if (!products || products.length === 0) {
+    return <p style={{ marginTop: 20 }}>No products yet</p>;
   }
 
   return (
-    <Table>
-      <TableHead>
-        <TableRow>
-          <TableCell>Name</TableCell>
-          <TableCell>Category</TableCell>
-          <TableCell>Price</TableCell>
-          <TableCell>Stock</TableCell>
-        </TableRow>
-      </TableHead>
-      <TableBody>
-        {products.map((p) => (
-          <TableRow key={p.id}>
-            <TableCell>{p.name}</TableCell>
-            <TableCell>{p.category}</TableCell>
-            <TableCell>₹{p.price}</TableCell>
-            <TableCell>{p.stock}</TableCell>
+    <Paper sx={{ mt: 3 }}>
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell><b>Name</b></TableCell>
+            <TableCell><b>Category</b></TableCell>
+            <TableCell><b>Price</b></TableCell>
+            <TableCell><b>Stock</b></TableCell>
           </TableRow>
-        ))}
-      </TableBody>
-    </Table>
+        </TableHead>
+
+        <TableBody>
+          {products.map((product) => (
+            <TableRow key={product.id}>
+              <TableCell>{product.name}</TableCell>
+              <TableCell>{product.category}</TableCell>
+              <TableCell>₹{product.price}</TableCell>
+              <TableCell>{product.stock}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </Paper>
   );
 }
